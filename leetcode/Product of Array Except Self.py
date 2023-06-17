9dfbf53b318f0e90aa827045d1a955a1f0d58d57
wayne chen <133223266+Wayne-chen0817@users.noreply.github.com>
2023-06-18 01:57:06 +0800

    Update Product of Array Except Self.py

--- leetcode/Product of Array Except Self.py
+++ leetcode/Product of Array Except Self.py
@@ -1,4 +1,3 @@
-nums = [1,2,3,4]
 '''
 Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
@@ -6,6 +5,8 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
 
 You must write an algorithm that runs in O(n) time and without using the division operation.
 '''
+nums = [1,2,3,4]
+
 def productExceptSelf(nums):
     
     answer, suf,pre = [1] * len(nums), 1, 1 #從後面開始乘和從前面開始乘
